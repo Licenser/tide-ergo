92822a6723f123b807e4064f8f201a32e6961d6c
Heinz N. Gies <heinz@licenser.net>
2020-06-08 10:50:44 +0200

    pretend

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ struct Counter {
 
 enum Error {
     Yaml(serde_yaml::Error),
+    Tide(tide::Error),
     DivisionByZero,
     DontLikeIt,
 }
@@ -20,6 +21,7 @@ impl TryInto<http_types::StatusCode> for &Error {
         match self {
             Error::Yaml(_) | Error::DivisionByZero => 422.try_into(),
             Error::DontLikeIt => 500.try_into(),
+            Error::Tide(t) => Ok(t.status()),
         }
     }
 }
@@ -28,6 +30,7 @@ impl Into<Body> for Error {
     fn into(self) -> Body {
         match self {
             Error::Yaml(y) => format!("YAML error: {}", y).into(),
+            Error::Tide(t) => format!("TIDE error: {}", t).into(),
             Error::DivisionByZero => "count can't be zero".into(),
             Error::DontLikeIt => "The server doesn't like this number".into(),
         }
@@ -47,6 +50,11 @@ impl From<serde_yaml::Error> for Error {
         Error::Yaml(e)
     }
 }
+impl From<tide::Error> for Error {
+    fn from(e: tide::Error) -> Self {
+        Error::Tide(e)
+    }
+}
 
 impl From<ergolib::Error> for Error {
     fn from(e: ergolib::Error) -> Self {
@@ -57,60 +65,45 @@ impl From<ergolib::Error> for Error {
     }
 }
 
-type Result = std::result::Result<Counter, Error>;
+type Result<T> = std::result::Result<T, Error>;
 
-fn handle42(mut counter: Counter) -> Result {
+fn handle42(mut counter: Counter) -> Result<Counter> {
     counter.count -= 1;
     counter.count = ergolib::nth42(counter.count)?;
     Ok(counter)
 }
 
-fn handle1337(mut counter: Counter) -> Result {
+fn handle1337(mut counter: Counter) -> Result<Counter> {
     counter.count -= 1;
     counter.count = ergolib::nth1337(counter.count)?;
     Ok(counter)
 }
 
-fn from_yaml(body: &str) -> std::result::Result<Counter, Error> {
-    Ok(serde_yaml::from_str(body)?)
+async fn get42(mut req: Request<()>) -> Result<Response> {
+    let body = req.body_string().await?;
+    let mut counter: Counter = serde_yaml::from_str(&body)?;
+    println!("[42] count is {}", counter.count);
+
+    counter = handle42(counter)?;
+    let mut res = Response::new(200);
+    res.set_body(Body::from_json(&counter)?);
+    Ok(res)
 }
 
+async fn get1337(mut req: Request<()>) -> Result<Response> {
+    let body = req.body_string().await?;
+    let mut counter: Counter = serde_yaml::from_str(&body)?;
+    println!("[1337] count is {}", counter.count);
+
+    counter = handle1337(counter)?;
+    let mut res = Response::new(200);
+    res.set_body(Body::from_json(&counter)?);
+    Ok(res)
+}
 #[async_std::main]
 async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let mut app = tide::new();
-    app.at("/42")
-        .get(|mut req: Request<()>| async move {
-            let body = req.body_string().await?;
-            // This isn't converted into a proper error despite being
-            // implemented for `Error` since we can't return a custom error type
-            let mut counter: Counter = serde_yaml::from_str(&body)?;
-            println!("[42] count is {}", counter.count);
-
-            match handle42(counter) {
-                Ok(c) => counter = c,
-                Err(e) => return Ok(e.into()),
-            }
-            let mut res = Response::new(200);
-            res.set_body(Body::from_json(&counter)?);
-            Ok(res)
-        })
-        .at("/1337")
-        .get(|mut req: Request<()>| async move {
-            let body = req.body_string().await?;
-            // This is a "workaround"
-            let mut counter: Counter = match from_yaml(&body) {
-                Ok(c) => c,
-                Err(e) => return Ok(e.into()),
-            };
-            println!("[1337] count is {}", counter.count);
-            match handle1337(counter) {
-                Ok(c) => counter = c,
-                Err(e) => return Ok(e.into()),
-            }
-            let mut res = Response::new(200);
-            res.set_body(Body::from_json(&counter)?);
-            Ok(res)
-        });
+    app.at("/42").get(get42).at("/1337").get(get1337);
     app.listen("127.0.0.1:8080").await?;
     Ok(())
 }
